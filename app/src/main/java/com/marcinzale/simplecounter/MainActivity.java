c0d0ca02124b8abe6e1fe0cde9b12c540322ae0e
Marcin <94755972+Marcinzale@users.noreply.github.com>
2023-02-26 22:34:58 +0100

    First commit

---
+++ app/src/main/java/com/marcinzale/simplecounter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.marcinzale.simplecounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView value;
+    int numberOfClicks = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment (View v) {
+        numberOfClicks++;
+
+        value = (TextView) findViewById(R.id.value);
+        value.setText(""+numberOfClicks);
+    }
+    public void decrement (View v) {
+        numberOfClicks--;
+
+        value = (TextView) findViewById(R.id.value);
+        value.setText(""+numberOfClicks);
+    }
+}
